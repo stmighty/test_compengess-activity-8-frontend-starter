77ea5aa04ed5a66def88ce4c786ff957d28ff8e9
Sirasit Tanrattanawong <might.la.fr@gmail.com>
2023-06-07 07:37:20 +0700

    updated code

--- script_cv.js
+++ script_cv.js
@@ -7,11 +7,11 @@ const authorizeApplication = () => {
 
 // TODO #3.1: Change group number
 const getGroupNumber = () => {
-  return 99;
+  return 38;
 };
 
 // Example: Send Get user profile ("GET") request to backend server and show the response on the webpage
-const getUserProfile = async () => {
+const getUserProfile = async () => {     //this is how to fetch()
   const options = {
     method: "GET",
     credentials: "include",
@@ -36,10 +36,35 @@ const getUserProfile = async () => {
 // TODO #3.3: Send Get Courses ("GET") request to backend server and filter the response to get Comp Eng Ess CV_cid
 //            and display the result on the webpage
 const getCompEngEssCid = async () => {
-  document.getElementById("ces-cid-value").innerHTML = "";
-  console.log(
-    "This function should fetch 'get courses' route from backend server and find cv_cid value of Comp Eng Ess."
-  );
+
+  const options = {
+    method: "GET",
+    credentials: "include",
+  };
+  await fetch(
+    `http://${backendIPAddress}/courseville//get_courses`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => data.data.student)
+    .then((course) => {
+      for(let i=0; i<course.length; i++)
+      {
+        if(course[i].course_no === "2110221")
+        {
+          document.getElementById("ces-cid-value").innerHTML = course[i].cv_cid;
+          break
+        }
+      }
+    })
+
+    .catch((error) => console.error(error));
+
+
+  //document.getElementById("ces-cid-value").innerHTML = "";
+  //console.log(
+  //  "This function should fetch 'get courses' route from backend server and find cv_cid value of Comp Eng Ess."
+  //);
 };
 
 // TODO #3.5: Send Get Course Assignments ("GET") request with cv_cid to backend server
@@ -49,9 +74,34 @@ const createCompEngEssAssignmentTable = async () => {
   table_body.innerHTML = "";
   const cv_cid = document.getElementById("ces-cid-value").innerHTML;
 
-  console.log(
-    "This function should fetch 'get course assignments' route from backend server and show assignments in the table."
-  );
+  // ต้อง fetch request ไปที่ตัว backend ก่อน
+  const options = {
+    method: "GET",
+    credentials: "include",
+  };
+
+  let items;
+  await fetch(
+    `http://${backendIPAddress}/courseville/get_course_assignments/${cv_cid}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      items = data.data})
+    .catch((error) => console.error(error));
+
+  items.map((item) => {
+    table_body.innerHTML += `
+      <tr id="${item.itemid}">      
+          <td>${item.itemid}</td>
+          <td>${item.title}</td>
+      </tr>
+      `;
+  })
+  //console.log(
+  //  "This function should fetch 'get course assignments' route from backend server and show assignments in the table."
+  //);
 };
 
 const logout = async () => {
--- script_item.js
+++ script_item.js
@@ -1,11 +1,16 @@
+/*
+
+const { error } = require("console");
+const { get } = require("http");
+
 // TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
-const backendIPAddress = "127.0.0.1:3000";
+const backendIPAddress = "127.0.0.1:3000";        //port of backend
 
 let itemsData;
 
 // TODO #2.1: Edit group number
 const getGroupNumber = () => {
-  return 99;
+  return 40;
 };
 
 // TODO #2.2: Show group members
@@ -15,20 +20,24 @@ const showGroupMembers = async () => {
   const member_dropdown = document.getElementById("name-to-add");
   member_dropdown.innerHTML =
     "<option value='0'>-- เลือกผู้ฝากซื้อ --</option>";
-  const options = {
+  const options = {               // ส่ง request แบบ get
     method: "GET",
-    credentials: "include",
+    credentials: "include",      // security reason
   };
   await fetch(`http://${backendIPAddress}/items/members`, options)
     .then((response) => response.json())
     .then((data) => {
-      const members = data;
+      const members = data;       //members is array of objects which have student_id and full_name as a key
       members.map((member) => {
-        member_list.innerHTML += `
+        member_list.innerHTML += `    
           <li>${member.full_name}</li>
-          `;
+          `;   // member_list.innerhtml += is change html code (add member)
         // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
-        member_dropdown.innerHTML += ``;
+        //member_dropdown.innerHTML += `<option value="${member.fullname}">${member.full_name}</option`;
+        const option = document.createElement('option');
+        option.value = member.full_name;
+        option.text = member.full_name;
+        member_dropdown.appendChild(option);
         // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
       });
     })
@@ -37,9 +46,13 @@ const showGroupMembers = async () => {
 
 // TODO #2.3: Send Get items ("GET") request to backend server and store the response in itemsData variable
 const getItemsFromDB = async () => {
+  
   console.log(
     "This function should fetch 'get items' route from backend server."
   );
+  
+  
+   
 };
 
 // TODO #2.4: Show items in table (Sort itemsData variable based on created_date in ascending order)
@@ -79,6 +92,165 @@ const deleteItem = async (item_id) => {
   console.log(
     "This function should fetch 'delete item' route in backend server and update items in the table."
   );
+};
+
+const redrawDOM = () => {
+  window.document.dispatchEvent(
+    new Event("DOMContentLoaded", {
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+  document.getElementById("item-to-add").value = "";
+  document.getElementById("name-to-add").value = "0";
+  document.getElementById("price-to-add").value = "";
+};
+
+document.getElementById("group-no").innerHTML = getGroupNumber();
+
+document.addEventListener("DOMContentLoaded", async function (event) {
+  console.log("Showing group members.");
+  await showGroupMembers();
+  console.log("Showing items from database.");
+  await getItemsFromDB();
+  showItemsInTable(itemsData);
+});
+
+*/
+
+
+// TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
+const backendIPAddress = "127.0.0.1:3000";
+
+let itemsData;
+
+// TODO #2.1: Edit group number                                                           //finish
+const getGroupNumber = () => {
+  return 38;
+};
+
+// TODO #2.2: Show group members                                                          //finish
+const showGroupMembers = async () => {
+  const member_list = document.getElementById("member-list");
+  member_list.innerHTML = "";
+  const member_dropdown = document.getElementById("name-to-add");
+  member_dropdown.innerHTML =
+    "<option value='0'>-- เลือกผู้ฝากซื้อ --</option>";
+  const options = {               // ส่ง request แบบ get
+    method: "GET",
+    credentials: "include",      // security reason
+  };
+  await fetch(`http://${backendIPAddress}/items/members`, options)
+    .then((response) => response.json())
+    .then((data) => {
+      const members = data;       //members is array of objects which have student_id and full_name as a key
+      members.map((member) => {
+        member_list.innerHTML += `    
+          <li>${member.full_name}</li>
+          `;   // member_list.innerhtml += is change html code (add member)
+        // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
+        //member_dropdown.innerHTML += `<option value="${member.fullname}">${member.full_name}</option`;
+        const option = document.createElement('option');
+        option.value = member.full_name;
+        option.text = member.full_name;
+        member_dropdown.appendChild(option);
+        // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
+      });
+    })
+    .catch((error) => console.error(error));
+};
+
+// TODO #2.3: Send Get items ("GET") request to backend server and store the response in itemsData variable
+const getItemsFromDB = async () => {
+  const options = {
+    method: "GET",
+    credentials: "include"
+  };
+  
+  await fetch(`http://${backendIPAddress}/items`, options)
+  .then((response) => response.json())
+  .then((data) => {
+    itemsData = data
+  })
+  .catch((error) => console.error(error));
+};
+
+// TODO #2.4: Show items in table (Sort itemsData variable based on created_date in ascending order)
+const showItemsInTable = (itemsData) => {
+  const table_body = document.getElementById("main-table-body");
+  table_body.innerHTML = "";
+  // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
+  itemsData.sort((a,b) => {                     //this is how to sort javascript object //https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
+    return a.created_date - b.created_date;
+  })
+  // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
+  itemsData.map((item) => {    //.map ก็เหมือน for loop แต่ละรอบ
+    // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //      //specify row with item_id
+    table_body.innerHTML += `
+        <tr id="${item.item_id}">      
+            <td>${item.item}</td>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
+            <td><button class="delete-row" onclick="deleteItem('${item.item_id}')">ลบ</button></td>
+        </tr>
+        `;        
+    // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
+  });
+};
+
+// TODO #2.5: Send Add an item ("POST") request to backend server and update items in the table
+const addItem = async () => {
+  const item = document.getElementById("item-to-add").value;
+  const name = document.getElementById("name-to-add").value;
+  const price = document.getElementById("price-to-add").value;
+
+
+  //create object that has 3 fields(item, name, price)
+  const itemToAdd = {
+    item: item,
+    name: name,
+    price: price
+  }
+
+  const options = {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(itemToAdd)
+  }
+
+
+  //we need to clear value of each variable
+  await fetch(`http://${backendIPAddress}/items`, options)
+    .then((response) =>
+    {
+      document.getElementById("item-to-add").value = "";
+      document.getElementById("name-to-add").value = 0;
+      const price = document.getElementById("price-to-add").value = "";
+    })
+    .catch((error) => console.error(error));
+
+    //we need to update the table
+    await getItemsFromDB();
+    showItemsInTable(itemsData);
+};
+
+// TODO 2.6: Send Delete an item ("DELETE") request to backend server and update items in the table
+const deleteItem = async (item_id) => {
+  const options = {
+    method: "DELETE",
+    credentials: "include"
+  }
+  await fetch(`http://${backendIPAddress}/items/${item_id}`, options)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => console.error(error));
+
+    await getItemsFromDB();
+    showItemsInTable(itemsData);
 };
 
 const redrawDOM = () => {
